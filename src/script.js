5e63d3efccd58d6498b30db995750e4650111ffa
GentleKyle <83362724+GentleKyle@users.noreply.github.com>
2025-01-06 10:32:19 -0700

    HTML JavaScript Completed

---
+++ src/script.js
@@ -0,0 +1,13 @@
+document.getElementById("jsTest").style.backgroundColor = "lime";
+
+/*
+function newPickleBackColor() {
+    let ele = document.getElementsByClassName("pickle");
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    for (let i = 0; i < ele.length; i++) {
+        ele[i].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    }
+}
+*/
