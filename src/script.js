41714857f05d06402408fcaf1edda63526d98162
GentleKyle <83362724+GentleKyle@users.noreply.github.com>
2025-01-08 15:22:46 -0700

    HTML Layout Completed

--- src/script.js
+++ src/script.js
@@ -16,6 +16,48 @@ function newPickleBackColor() {
     }
 }
 
+function gamble(ele) {
+    let emojis = [127799, 127795, 127808, 127812, 129365]; 
+    let randE = Math.floor(Math.random() * emojis.length);
 
+    const newEle = document.createElement("div");
+    newEle.textContent = String.fromCodePoint(emojis[randE]);
+    newEle.className = "emoji";
+    ele.replaceWith(newEle);
+}
+
+function reload() {
+    location.reload();
+}
+
+
+//CAN IMPROVE - enough for now - add columns/win msg on third match/other than alert(popover?)/alert once
+function outcomeAlert(grids) {
+    let grads = grids.getElementsByClassName("grad");
+    let emojis = grids.getElementsByClassName("emoji");
+    let gameObj = {
+        matchNum: 3,
+        arrData: emojis,
+        matchCount: 0,
+    };
+    
+    if (!grids.contains(grads[0])) {
+        for (let i = 0; i < emojis.length; i++) {
+            let lookBack = i % gameObj.matchNum;
+            console.log(emojis[i].innerHTML);
+            console.log(emojis[i]);
+            if (lookBack === 0) {
+                gameObj.matchCount = 1;
+            } 
+            else if (emojis[i].innerHTML === emojis[i - lookBack].innerHTML) {
+                gameObj.matchCount++;
+            }
+            if (gameObj.matchCount === gameObj.matchNum) {
+                return alert("You Win!!");
+            }  
+        }    
+        return alert("You Lose!!");
+    } 
+}
 
 
